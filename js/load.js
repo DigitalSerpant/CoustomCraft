846efefb1ce0abac5f085fa576e20908921a3ca2
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-29 09:27:19 -0900

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
 const el = document.getElementById('filler'); setTimeout(() => { el.style.visibility = 'visible'; }, 5000); function loadGame() {
-    var URL = document.getElementById("dropDown").value; var baseHTML = '<div>' +
-        '<iframe style="border: solid 1px orange" width="100%" height="100%" allow="clipboard-read; clipboard-write" id="game" src="' + URL + '" scrolling="no"></iframe></div>'; document.getElementById("filler").innerHTML = baseHTML;
+    var URL = document.getElementById("dropDown").value;
+    window.location.href = URL;        
 }
