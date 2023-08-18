239f2425a29c55b24145ee4f84fc0b6b1bdda8aa
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-08-18 14:48:04 -0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -2,13 +2,16 @@ const el = document.getElementById('filler'); setTimeout(() => { el.style.visibi
     var URL = document.getElementById("dropDown").value;
     openGame(URL);       
 }
-    function openGame(url) {
-      const win = window.open('about:blank', '_blank', 'menubar=no, status=no, toolbar=no, resizable=no, width=' + window.screen.width + ', height=' + window.screen.height + ', titlebar=no, alwaysRaised=yes');
-  
-      win.document.write('<!DOCTYPE html><html><head><title>Coustom Craft</title></head><body style="margin: 0; padding: 0;">');
-      win.document.write('<iframe id="sus" src="' + url + '" frameborder="0" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%;"></iframe>');
-      win.document.write('</body></html>');
+function openGame(url) {
+  const win = window.open('about:blank', '_blank', 'menubar=no, status=no, toolbar=no, resizable=no, width=' + window.screen.width + ', height=' + window.screen.height + ', titlebar=no, alwaysRaised=yes');
+
+  fetch('https://raw.githubusercontent.com/DigitalSerpant/CoustomCraft/main/FuschiaX.html')
+    .then(response => response.text())
+    .then(htmlContent => {
+      win.document.write(htmlContent);
       win.document.close();
-    }
+    });
+}
+
 window.myIframe = document.getElementById("sus").contentWindow;
 window.addEventListener("keydown", function(evt) { myIframe.myKeydownEventHandler(evt); });
