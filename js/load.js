8237bc0364c30a51cb6c3f298fce9c238913e186
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-09-30 20:13:58 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,28 +1,23 @@
-const el = document.getElementById('filler'); setTimeout(() => { el.style.visibility = 'visible'; }, 5000); function loadGame() {
-    var URL = document.getElementById("dropDown").value;
-    openGame(URL);       
-}
-function openGame(url) {
-  const win = window.open('about:blank', '_blank', 'menubar=no, status=no, toolbar=no, resizable=no, width=' + window.screen.width + ', height=' + window.screen.height + ', titlebar=no, alwaysRaised=yes');
+const el = document.getElementById('filler');
+setTimeout(() => {
+    el.style.visibility = 'visible';
+}, 5000);
 
-if(url==="3"){
-  fetch("https://digitalserpant.github.io/CoustomCraft/FuschiaX.html")
-    .then(response => response.text())
-    .then(htmlContent => {
-      win.document.write(htmlContent);
-      win.document.close();
-    });
-}
-    else{
-  fetch("try.html")
-    .then(response => response.text())
-    .then(htmlContent => {
-      win.document.write(htmlContent);
-      win.document.close();
-    });
+function loadGame() {
+    const win = window.open('about:blank', '_blank', 'menubar=no, status=no, toolbar=no, resizable=no, width=' + window.screen.width + ', height=' + window.screen.height + ', titlebar=no, alwaysRaised=no');
+
+        fetch("try.html")
+            .then(response => response.text())
+            .then(htmlContent => {
+                // Set chrome property to undefined in the opened window
+                win.chrome = undefined;
+
+                win.document.write(htmlContent);
+                win.document.close();
+            });
     }
 
-}
-
 window.myIframe = document.getElementById("sus").contentWindow;
-window.addEventListener("keydown", function(evt) { myIframe.myKeydownEventHandler(evt); });
+window.addEventListener("keydown", function (evt) {
+    myIframe.myKeydownEventHandler(evt);
+});
